9900b3c621a65560842ed66afeab27d08200906f
Stas2201 <max51570@mail.ru>
2026-09-10 13:23:42 +0300

    Clean up config.py by removing comments
    Removed comments and cleaned up the config file.

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
 import os
 
-# Читаем порт из Render
 PORT = int(os.getenv("PORT", 8443))
 
-# Читаем секрет из Render
 USERS = {
     "tg": os.getenv("SECRET", "00000000000000000000000000000001")
 }
 
-# Прокси должен слушать весь интернет, иначе Render не увидит порт
 LISTEN_ADDR = "0.0.0.0"
 
 MODES = {
@@ -17,5 +14,4 @@ MODES = {
     "tls": True
 }
 
-# Домен для TLS
 TLS_DOMAIN = os.getenv("TLS_DOMAIN", "www.cloudflare.com")
